3358eb74074b975533d0e0abe4e221e5ca25e79a
willington <99karencordoba@gmail.com>
2021-11-10 17:20:46 -0500

    newJs

--- js/7_cadenaTexto.js
+++ js/7_cadenaTexto.js
@@ -3,3 +3,8 @@ var municipio = "pITalITo";
 municipio = municipio.toLowerCase();
 municipio = municipio[0].toUpperCase() + municipio.slice(1);
 console.log(municipio);
+
+var SegundoNombre = 'Valle de Laboyos - Pitalito';
+SegundoNombre = SegundoNombre.slice(9);
+SegundoNombre = SegundoNombre.slice(0, 8);
+console.log(SegundoNombre);
